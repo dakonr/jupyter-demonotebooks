088618c1271050a916b96fe98872ac8e6b26a80b
Daniel Konrad <mail@daniel-konrad.com>
2022-12-26 14:45:07 +0100

    added response models and enums

--- ml/titanic/app/app.py
+++ ml/titanic/app/app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Body
 from enum import IntEnum, Enum
 from joblib import load
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List
 
 classfier = load("./model/titanic.joblib")
 
@@ -13,24 +15,54 @@ app = FastAPI(
 
 # Enumerations
 class PassengerClass(IntEnum):
+    """Passenger Class
+    1 Cl, 2 Cl, 3 Cl
+    Args:
+        IntEnum (int): Passenger Class
+    """
+
     first = 1
     second = 2
     third = 3
 
 
 class GenderClass(Enum):
+    """Gender Selector
+
+    Args:
+        Enum (String): Gender of the person, choos between male and female
+    """
+
     female = "female"
     male = "male"
 
 
 class EmbarkedClass(Enum):
+    """Embarked Class
+
+    Args:
+        Enum (string): Embarked Station
+    """
+
     S = "S"
     C = "C"
     Q = "Q"
 
 
-# Pclass	Sex	Age	SibSp	Parch	Fare	Embarked
-@app.get("/predict", tags=["predict"])
+# Response Model
+class PredictionResponse(BaseModel):
+    passenger_class: int
+    sex: int
+    age: float
+    sibsp: int
+    parch: int
+    fare: float
+    embarked: int
+    prediction: int
+    log_proba: List
+
+
+@app.get("/predict", tags=["predict"], response_model=PredictionResponse)
 async def get_prediction(
     passenger_class: PassengerClass,
     sex: GenderClass,
@@ -39,7 +71,7 @@ async def get_prediction(
     parch: int,
     fare: float,
     embarked: EmbarkedClass,
-):
+) -> PredictionResponse:
     processed_sex = dict({"female": 0, "male": 1}).get(sex.value)
     processed_embarked = dict({"S": 3, "C": 2, "Q": 1}).get(embarked.value)
     prediction = classfier.predict(
@@ -69,16 +101,14 @@ async def get_prediction(
         ]
     ).tolist()
 
-    return JSONResponse(
-        {
-            "PassengerClass": passenger_class.value,
-            "sex": processed_sex,
-            "age": age,
-            "sibsp": sibsp,
-            "parch": parch,
-            "fare": fare,
-            "embarked": processed_embarked,
-            "prediction": prediction[0],
-            "log_proba": log_proba,
-        }
+    return PredictionResponse(
+        passenger_class=passenger_class.value,
+        sex=processed_sex,
+        age=age,
+        sibsp=sibsp,
+        parch=parch,
+        fare=fare,
+        embarked=processed_embarked,
+        prediction=prediction[0],
+        log_proba=log_proba,
     )
